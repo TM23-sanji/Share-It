a59e64afd0634b09516751f5dd903089d7597ad6
TM23-sanji <tusharmishra802@gmail.com>
2025-05-25 03:53:08 +0530

    test-XIII

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,11 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "**/lib**",
-    ],
-  },
+  // {
+  //   ignores: [
+  //     "**/lib**",
+  //   ],
+  // },
 ];
 
 export default eslintConfig;
